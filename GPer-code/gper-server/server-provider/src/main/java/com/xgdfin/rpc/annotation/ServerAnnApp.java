3a33628fafa8778e2ad749317662d1f8d4dcaa2a
hongjun xie <Too_Xhongjun@163.com>
2019-10-17 15:23:24 +0800

    RPC作业提交
    基础RPC作业提交

---
+++ GPer-code/gper-server/server-provider/src/main/java/com/xgdfin/rpc/annotation/ServerAnnApp.java
@@ -0,0 +1,18 @@
+package com.xgdfin.rpc.annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class ServerAnnApp {
+    public static void main( String[] args ) throws IOException {
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(RpcServerConfig.class);
+        ((AnnotationConfigApplicationContext) context).start();
+    }
+}
---
+++ GPer-code/gper-server/server-provider/src/main/java/com/xgdfin/rpc/base/ServerBaseApp.java
@@ -0,0 +1,21 @@
+package com.xgdfin.rpc.base;
+
+
+import com.xgdfin.rpc.IRpcServerApi;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class ServerBaseApp {
+    public static void main( String[] args ) throws IOException {
+
+        IRpcServerApi rpcServer = new RpcServerApiImpl();
+        RpcServerProxy serverProxy = new RpcServerProxy();
+        System.out.println("Server start ... ");
+        serverProxy.publisher(8083, rpcServer);
+
+    }
+}
