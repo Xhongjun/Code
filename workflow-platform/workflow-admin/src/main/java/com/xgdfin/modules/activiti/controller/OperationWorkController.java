881efd8c5e1c3318f01957a263eb461fdd5988b8
hongjun.xie <Too_Xhongjun@163.com>
2019-12-04 12:15:28 +0800

    bean 提交

---
+++ workflow-platform/workflow-admin/src/main/java/com/xgdfin/modules/activiti/controller/OperationWorkController.java
@@ -0,0 +1,1 @@
+package com.xgdfin.modules.activiti.controller;/** Copyright (C) 2017  ShenZhen xinLianZhengXin Co.,Ltd All Rights Reserved.* 未经本公司正式书面同意，其他任何个人、团体不得使用、复制、修改或发布本软件.* 版权所有深圳市信联征信有限公司 http://www.credlink.com/*//** * <p>运营部门上线流程控制器</p> * * @author  xiehongjun * @date    17:36 2019/12/1 */public class OperationWorkController {}
